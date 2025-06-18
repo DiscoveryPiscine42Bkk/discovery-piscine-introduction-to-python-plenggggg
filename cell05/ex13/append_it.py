2f16421cf64caad481912fe56542296b03e92f1e
plenggggg <pthittakhemngam@gmail.com>
2025-06-18 10:49:45 +0000

    append_it.py

---
+++ cell05/ex13/append_it.py
@@ -0,0 +1,11 @@
+import sys
+def append_it():
+    if len(sys.argv) == 1:
+        print("none")
+        return
+
+    for arg in sys.argv[1:]:
+        if not arg.endswith("ism"):
+           print(f"{arg}ism")
+if __name__ == "__main__":
+    append_it()
